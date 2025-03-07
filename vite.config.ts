f04998cf5c9c2f69cc0770f76eb26640d6a3922f
Nuno Fernandes <nuno.o.fernandes@outlook.pt>
2025-03-07 09:39:36 +0000

    Refactor App component to use React Router and add Home page; configure Tailwind CSS and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-  }
+    plugins: [
+        tailwindcss(),
+        react(),
+    ],
+    server: {
+        port: 3000,
+        strictPort: true,
+        host: true,
+    },
+    base: "/askksa-web/",
 })
